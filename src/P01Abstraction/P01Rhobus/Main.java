c052d8d05a44dfaf0d23cab98d59a7384ab9ade1
Denis Pasha <111784197+DenisPasha@users.noreply.github.com>
2023-01-12 13:58:13 +0000

    update

---
+++ src/P01Abstraction/P01Rhobus/Main.java
@@ -0,0 +1,39 @@
+package P01Abstraction.P01Rhobus;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+
+        rhombusFirstHalf(n);
+
+
+        rhombusSecondHalf(n);
+    }
+
+    private static void rhombusSecondHalf(int n) {
+        for (int i = n -1; i >=1 ; i--) {
+            printStars(n, i);
+        }
+    }
+
+    private static void rhombusFirstHalf(int n) {
+        for (int i = 1; i <= n; i++) {
+            printStars(n, i);
+        }
+    }
+
+    private static void printStars(int n, int i) {
+        System.out.println();
+        for (int j = 1; j <= n - i; j++) {
+            System.out.print(" ");
+        }
+        for (int j = 1; j <= i; j++) {
+            System.out.print("* ");
+        }
+    }
+}
